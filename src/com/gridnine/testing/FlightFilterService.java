c9b41e5a96d09068936b43e933fdd390314bbc17
gface <89259112970@mail.ru>
2025-04-03 10:52:14 +0300

    add: FlightFilterService

---
+++ src/com/gridnine/testing/FlightFilterService.java
@@ -0,0 +1,19 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+public class FlightFilterService {
+    private final List<FlightFilter> filters;
+
+    public FlightFilterService(List<FlightFilter> filters) {
+        this.filters = filters;
+    }
+
+    public List<Flight> filterFlights(List<Flight> flights) {
+        List<Flight> filteredFlights = flights;
+        for (FlightFilter filter : filters) {
+            filteredFlights = filter.filter(filteredFlights);
+        }
+        return filteredFlights;
+    }
+}
---
+++ src/com/gridnine/testing/GroundTimeExceedsTwoHoursFilter.java
@@ -0,0 +1,19 @@
+package com.gridnine.testing;
+
+import java.util.List;
+
+public class GroundTimeExceedsTwoHoursFilter implements FlightFilter {
+    @Override
+    public List<Flight> filter(List<Flight> flights) {
+        return flights.stream()
+                .filter(flight -> {
+                    List<Segment> segments = flight.getSegments();
+                    long totalGroundTime = 0;
+                    for (int i = 0; i < segments.size() - 1; i++) {
+                        totalGroundTime += segments.get(i + 1).getDepartureDate().until(segments.get(i).getArrivalDate(), java.time.temporal.ChronoUnit.HOURS);
+                    }
+                    return totalGroundTime <= 2;
+                })
+                .toList();
+    }
+}
